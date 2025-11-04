92da4d411cdec08454284dba018072d8f7abde21
Josh <j.wielings@gmail.com>
2025-11-04 13:43:29 +0100

    Created Team model, updated AppUser model to accomodate

--- Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/models/Team.java
+++ Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/models/Team.java
@@ -1,7 +1,24 @@
 package Dreamteam.GroupProject1.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "teams")
+    private List<AppUser> teamMembers;
+
+
 }
