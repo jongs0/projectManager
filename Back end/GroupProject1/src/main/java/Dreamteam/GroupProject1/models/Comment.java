93d512fbdb66f904e2f41a358c021c32c6d98a13
NHanssens <120455517+N-Hanssens@users.noreply.github.com>
2025-11-04 12:48:52 +0100

    Created comment model

--- Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/models/Comment.java
+++ Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/models/Comment.java
@@ -1,7 +1,43 @@
 package Dreamteam.GroupProject1.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Comment {
+    @GeneratedValue
+    @Id
+    private Long id;
+    private String body;
+    @OneToOne
+    private AppUser user;
+    @OneToOne
+    private Task task;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public AppUser getUser() { return user; }
+
+    public void setUser(AppUser user) {
+        this.user = user;
+    }
+
+    public Task getTask() { return task; }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
 }
