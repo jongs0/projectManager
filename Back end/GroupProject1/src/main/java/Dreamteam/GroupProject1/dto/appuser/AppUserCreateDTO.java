92644bb8e2f90321ca50043b890b548662825921
Josh <j.wielings@gmail.com>
2025-11-05 13:53:23 +0100

    Added AppUser create and summary dto's

--- Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/dto/appuser/AppUserCreateDTO.java
+++ Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/dto/appuser/AppUserCreateDTO.java
@@ -1,4 +1,19 @@
 package Dreamteam.GroupProject1.dto.appuser;
 
-public record AppUserCreateDTO() {
+import Dreamteam.GroupProject1.models.AppUser;
+import Dreamteam.GroupProject1.models.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
+public record AppUserCreateDTO(
+
+        @Email String email, String password
+
+) {
+    public AppUser toEntity() {
+        AppUser appUser = new AppUser();
+        appUser.setEmail(this.email);
+        appUser.setPassword(this.password);
+        return appUser;
+    }
 }
--- Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/dto/appuser/AppUserSummaryDTO.java
+++ Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/dto/appuser/AppUserSummaryDTO.java
@@ -1,4 +1,17 @@
 package Dreamteam.GroupProject1.dto.appuser;
 
-public record AppUserSummaryDTO() {
+import Dreamteam.GroupProject1.models.AppUser;
+
+public record AppUserSummaryDTO(
+
+        Long id,
+        String email
+
+) {
+    public static AppUserSummaryDTO fromEntity(AppUser appUser) {
+        return new AppUserSummaryDTO(
+                appUser.getId(),
+                appUser.getEmail()
+        );
+    }
 }
