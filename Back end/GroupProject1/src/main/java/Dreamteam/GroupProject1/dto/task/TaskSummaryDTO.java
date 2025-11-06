1af8191b29efc89810cd4944991dffac96eb6003
Josh <j.wielings@gmail.com>
2025-11-06 12:57:24 +0100

    updated task summary dto to include a list of watchers

--- Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/dto/task/TaskSummaryDTO.java
+++ Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/dto/task/TaskSummaryDTO.java
@@ -1,19 +1,31 @@
 package Dreamteam.GroupProject1.dto.task;
 
+import Dreamteam.GroupProject1.dto.appuser.AppUserSummaryDTO;
 import Dreamteam.GroupProject1.models.Task;
+
+import java.util.List;
 
 public record TaskSummaryDTO(
         Long id,
         String name,
-        String body
+        String body,
+        List<AppUserSummaryDTO> watchers
 ) {
 
     public static TaskSummaryDTO fromEntity(Task task) {
 
+        List<AppUserSummaryDTO> watcherDtos = task.getUsers() == null
+                ? List.of()
+                : task.getUsers().stream()
+                .map(AppUserSummaryDTO::fromEntity)
+                .toList();
+
         return new TaskSummaryDTO(
                 task.getId(),
                 task.getName(),
-                task.getBody()
+                task.getBody(),
+                watcherDtos
+
         );
     }
 }
