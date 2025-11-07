0eb9cfca7c63da3f194720f6a211fefdee305686
Josh <j.wielings@gmail.com>
2025-11-07 10:27:28 +0100

    Fixed repo find by method, was in the wrong one

--- Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/repository/ProjectRepository.java
+++ Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/repository/ProjectRepository.java
@@ -9,7 +9,5 @@ import java.util.List;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-    List<AppUser> findByEmail(String email);
-    boolean existsByEmail(String email);
 
 }
