a9745d9fc10fe37d0ea61ec2e9debb21dfadb525
Josh <j.wielings@gmail.com>
2025-11-07 10:19:58 +0100

    Added find and exist by email for repository

--- Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/repository/ProjectRepository.java
+++ Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/repository/ProjectRepository.java
@@ -1,10 +1,15 @@
 package Dreamteam.GroupProject1.repository;
 
+import Dreamteam.GroupProject1.models.AppUser;
 import Dreamteam.GroupProject1.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<AppUser> findByEmail(String email);
+    boolean existsByEmail(String email);
 
 }
