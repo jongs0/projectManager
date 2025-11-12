f412932623ba4daac10fe0f5943cf144c383fcaa
Sietse de Jong <sietse.dejong68@gmail.com>
2025-11-12 12:19:23 +0100

    Added create user method in controller

--- Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/controllers/UserController.java
+++ Back end/GroupProject1/src/main/java/Dreamteam/GroupProject1/controllers/UserController.java
@@ -1,10 +1,29 @@
 package Dreamteam.GroupProject1.controllers;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import Dreamteam.GroupProject1.dto.appuser.AppUserCreateDTO;
+import Dreamteam.GroupProject1.dto.appuser.AppUserDTO;
+import Dreamteam.GroupProject1.models.AppUser;
+import Dreamteam.GroupProject1.service.AppUserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
-//@RequestMapping("/pathhier") TBD!!
+@RequestMapping("users")
 public class UserController {
+    final private AppUserService userService;
+
+    @Autowired
+    public UserController(AppUserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AppUserDTO> createUser(@Valid @RequestBody AppUserCreateDTO createDto) {
+
+        AppUserDTO created = userService.createAppUser(createDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
 }
