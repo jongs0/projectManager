bfe74ff97f018d1cd53f324d10e955a9ec065cf1
Josh <j.wielings@gmail.com>
2025-11-19 13:12:33 +0100

    added role to userstore

--- Front end/GroupProject/src/stores/userStore.ts
+++ Front end/GroupProject/src/stores/userStore.ts
@@ -1,21 +1,25 @@
 import { createStore } from "@odemian/react-store";
+import type { Role } from "../types/models.js";
 
 interface User {
     email: String
     password: String
-    id: number | null;
+    id: number | null
+    role: Role | null;
 }
 
 export const [currentUser, updateUser] = createStore<User>({
     email: "",
     password: "",
-    id: null
+    id: null,
+    role: null
 });
 
 export const logout = () => {
     updateUser({
         email: "",
         password: "",
-        id: null
+        id: null,
+        role: null
     });
 };
